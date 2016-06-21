b0d9cd3f4d110067a14abb859ddab71256475657
bitores <773155801@qq.com>
2016-06-22 00:06:41 +0800

    M

---
+++ lesson03/app/main.js
@@ -0,0 +1,6 @@
+import './main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './component.jsx';
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
